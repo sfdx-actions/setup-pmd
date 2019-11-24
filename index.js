854ccc2ed980f257d6a8b7d26cbc44f6e7c2ea3f
fechanique <felipe.echanique@gmail.com>
2019-11-24 05:10:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function installPMD(){
   var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.19.0/pmd-bin-6.19.0.zip -P /tmp'
   var unzip = 'unzip /tmp/pmd-bin-6.19.0.zip -d /tmp'
   var shopt = 'shopt -s expand_aliases'
-  var alias = 'alias pmd="/tmp/pmd-bin-6.19.0/bin/run.sh pmd"'
+  var alias = 'echo "pmd=\'/tmp/pmd-bin-6.19.0/bin/run.sh pmd\'" >> ~/.bash_aliases && source ~/.bash_aliases'
   exec(download+' && '+unzip+' && '+shopt+' && '+alias, function(error, stdout, stderr){
     if(error) console.log(stderr)
 	console.log(stdout)
