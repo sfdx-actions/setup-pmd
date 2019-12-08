03cad9c91a02807a59d99b4562d5815078b7574f
fechanique <felipe.echanique@gmail.com>
2019-12-08 16:00:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function installPMD(){
 function referencePMD(){
   var cmd = 
 `echo '#! /bin/sh
-$HOME/pmd/run.sh pmd "$@"' > /user/bin/pmd
+$HOME/pmd/run.sh pmd "$@"' > /usr/bin/pmd
 `
   exec('sudo '+cmd, function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
