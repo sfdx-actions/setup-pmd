d2ac7e7c5cc235aa4652386e4fc9d2ed7da44a48
fechanique <felipe.echanique@gmail.com>
2019-12-08 17:02:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function installPMD(){
 }
 
 function referencePMD(){
-  var mk = 'sudo mkdir /snap/bin'
+  var mk = 'sudo mkdir /snap/bin && sudo chmod -R 757 /snap/bin'
   var pt = `echo "export PATH=$PATH:$HOME/bin/" >> $HOME/.bashrc`
   var rn = `. "$HOME/.bashrc"`
   var cmd = 
