2db24fa1c6f86eaf071d387bfd88d4c0097a01a1
Jamie Briers <Jamie.Briers@sage.com>
2021-03-22 11:25:52 +0000

    take a version

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const core = require('@actions/core')
 const exec = require('child_process').exec
 
 try {
-  installPMD()
+  let version = core.getInput('pmd-version');
+  installPMD(version)
 } catch (error) {
   core.setFailed(error.message)
 }
 
-function installPMD(){
-  var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.19.0/pmd-bin-6.19.0.zip -P /tmp'
-  var unzip = 'unzip /tmp/pmd-bin-6.19.0.zip -d /tmp'
+function installPMD(version){
+  var download = `wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F${version}/pmd-bin-${version}.zip -P /tmp -O pmd.zip`
+  var unzip = 'unzip /tmp/pmd.zip -d /tmp'
   var mk = 'mkdir $HOME/pmd'
-  var mv = 'mv /tmp/pmd-bin-6.19.0/* $HOME/pmd'
+  var mv = 'mv /tmp/pmd/* $HOME/pmd'
   exec(download+' && '+unzip+' && '+mk+' && '+mv, function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
     core.debug(stdout)
